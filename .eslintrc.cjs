97bf746d85420d551a40fa7793864cb5646a260f
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-06-26 00:20:30 +0200

    add ECMA 13 support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,4 +4,5 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+  parserOptions: { ecmaVersion: 13 },
 };
